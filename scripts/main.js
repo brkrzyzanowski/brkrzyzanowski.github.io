3f6ce21a4a5bf367b4b411b1d1e1795be2ef1c86
Zach Krzyzanowski <zkrzyz@gmail.com>
2016-06-18 13:30:03 -0400

    Added nav and main partials

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
-  Handlebars.registerPartial("index", '{{index}}');
-  var source   = $("#index-template").html();
+$(document).ready(function () {
+  var source = $('#index-template').html();
   var template = Handlebars.compile(source);
+  Handlebars.registerPartial('index', document.getElementById('index-partial').innerHTML);
+  Handlebars.registerPartial('nav', document.getElementById('nav-partial').innerHTML);
   $(document.body).append(template());
 });
